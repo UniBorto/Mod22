28636594aa425cb582dac1e90a869fe5e8abd3ef
Lorenzo <lorenzo.202212195@unilasalle.edu.br>
2026-04-06 13:15:49 -0300

    Erro classe Pessoa.java corrigido

---
+++ src/main/Pessoa.java
@@ -0,0 +1,37 @@
+package main;
+
+public class Pessoa {
+    public String nome;
+    public String genero;
+    public int idade;
+
+    public Pessoa(String nome, String genero, int idade) {
+        this.nome = nome;
+        this.genero = genero;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
